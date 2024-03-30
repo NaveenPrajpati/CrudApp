9db6f0cf811a2d91b457da583d9121200145e075
NaveenPrajapati <solidauto202@gmail.com>
2024-03-30 14:50:50 +0530

    backend url updated

--- src/services/endPoints.js
+++ src/services/endPoints.js
@@ -1,5 +1,5 @@
 // const baseurl = "http://localhost:5000/task/";
-const baseurl = "https://crudapp-oh9m.onrender.com/";
+const baseurl = "https://crudapp-oh9m.onrender.com/task/";
 export const addApi = baseurl + "add";
 export const getApi = baseurl + "getAll";
 export const removeApi = (id) => baseurl + `remove/${id}`;
